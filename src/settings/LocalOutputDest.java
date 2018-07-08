22ad0525f321758f2f27cc3cdc4700979108455e
mutchun <kc88930@gmail.com>
2018-07-07 23:17:26 -0600

    before ignore

--- src/settings/LocalOutputDest.java
+++ src/settings/LocalOutputDest.java
@@ -1,8 +1,8 @@
 package settings;
 
 public class LocalOutputDest {
-    private static String LOCAL_GOOGLE_DRIVE_ROOT_FOLDER = "C:/Users/cloud/Google Drive";    // change this to local Google drive ROOT folder
+    private static String LOCAL_GOOGLE_DRIVE_ROOT_FOLDER = "E:\\Google Drive Tacc";    // change this to local Google drive ROOT folder
 
     public static String SCRAPE_DEST = LOCAL_GOOGLE_DRIVE_ROOT_FOLDER + "/Code/scrape_results";
-    public static String db_dest = LOCAL_GOOGLE_DRIVE_ROOT_FOLDER + "/Code/TAAC_Scraper_Database";
+    public static String DB_DEST = LOCAL_GOOGLE_DRIVE_ROOT_FOLDER + "/Code/TAAC_Scraper_Database/TACC_DB.accdb";
 }
