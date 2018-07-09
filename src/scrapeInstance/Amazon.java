3c5d1b4f6a0c99f5ef9d62bd804e3e3cc113946b
mutchun <kc88930@gmail.com>
2018-07-08 20:43:04 -0600

    change rating return type

--- src/scrapeInstance/Amazon.java
+++ src/scrapeInstance/Amazon.java
@@ -16,7 +16,9 @@ public class Amazon extends ScrapeUtil {
 
 	private String marketplace, url, asin;
 	private Document amazonPage;
-	private String productTitle = "", rating = "", reviews = "", answeredQ = "";
+	private String productTitle = "";
+	private Double rating = 0.0;
+	private int reviews = 0, answeredQ = 0;
 	private Double price = 0.0, savingsDollar = 0.0;
 	private int savingsPercentage = 0;
 	private String coupon = "", promo = "";
@@ -62,10 +64,10 @@ public class Amazon extends ScrapeUtil {
 
 		//constant fields
 		productTitle = amazonPage.select("span#productTitle").text().replace(",", "");;
-		rating = amazonPage.select("div#averageCustomerReviews").select("span#acrPopover").attr("title").replace(" out of 5 stars", "");
-		try { reviews = amazonPage.select("span#acrCustomerReviewText, span[data-hook=\"total-review-count\"]").first().text().replaceAll("[^\\d]", "");
-		} catch (NullPointerException ne) { reviews = "0"; }
-		answeredQ = amazonPage.select("a#askATFLink > span").text().replaceAll("[^\\d]", "");
+		rating = Double.parseDouble(amazonPage.select("div#averageCustomerReviews").select("span#acrPopover").attr("title").replace(" out of 5 stars", ""));
+		try { reviews = Integer.parseInt(amazonPage.select("span#acrCustomerReviewText, span[data-hook=\"total-review-count\"]").first().text().replaceAll("[^\\d]", ""));
+		} catch (NullPointerException ne) { reviews = 0; }
+		answeredQ = Integer.parseInt(amazonPage.select("a#askATFLink > span").text().replaceAll("[^\\d]", ""));
 		String priceStr = amazonPage.select("span#priceblock_ourprice, span#priceblock_dealprice").text().replaceAll("\\$|\\,", "");
 		if(priceStr.equals("")) {	//most likely in the case of scraping Books
 			priceStr = amazonPage.select("span[class=\"a-size-medium a-color-price offer-price a-text-normal\"], span[class=\"a-size-medium a-color-price header-price\"]").text().replaceAll("\\$|\\,", "");
@@ -202,27 +204,27 @@ public class Amazon extends ScrapeUtil {
 		this.productTitle = productTitle;
 	}
 
-	public String getRating() {
+	public Double getRating() {
 		return rating;
 	}
 
-	public void setRating(String rating) {
+	public void setRating(Double rating) {
 		this.rating = rating;
 	}
 
-	public String getReviews() {
+	public int getReviews() {
 		return reviews;
 	}
 
-	public void setReviews(String reviews) {
+	public void setReviews(int reviews) {
 		this.reviews = reviews;
 	}
 
-	public String getAnsweredQ() {
+	public int getAnsweredQ() {
 		return answeredQ;
 	}
 
-	public void setAnsweredQ(String answeredQ) {
+	public void setAnsweredQ(int answeredQ) {
 		this.answeredQ = answeredQ;
 	}
 
